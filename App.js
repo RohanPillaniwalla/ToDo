9b4f442aa8e2872fd2ae0bbde2c8b40719accd8c
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2023-04-13 19:08:59 +0530

    basic todo implementation
    A very basic implementation to add and show todos only

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// TODO APP
+// take input from user
+// add button adds the task in a list
+// display the tasks
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [todo, setTodo] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	const handleAddTodo = () => {
+		setTodos([...todos, todo]);
+		setTodo('');
+	};
+
+	return (
+		<View style={styles.container}>
+			<TextInput
+				placeholder="Add Todo"
+				value={todo}
+				onChangeText={(text) => setTodo(text)}
+			/>
+			<TouchableOpacity onPress={handleAddTodo}>
+				<Text>Add</Text>
+			</TouchableOpacity>
+
+			{todos.map((todo, index) => (
+				<Text key={index}>{todo}</Text>
+			))}
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
